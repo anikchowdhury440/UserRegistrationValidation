f00cec2b3deb1238cab4b69f894b1eb52351c5fc
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-09-09 15:37:52 +0530

    User Last Name Validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 #pattern
-pattern_fname="^[[:upper:]][[:lower:]]{2,}$"
+pattern_name="^[[:upper:]][[:lower:]]{2,}$"
 
 #First name check
 fname_check=0
@@ -9,7 +9,7 @@ echo "Enter first name starts with Caps and has minimum 3 Characters"
 while [[ fname_check -eq 0 ]]
 do
         read -p "Enter First name: " fname
-        if [[ $fname =~ $pattern_fname ]]
+        if [[ $fname =~ $pattern_name ]]
         then
                 fname_check=1
         else
@@ -17,6 +17,22 @@ do
         fi
 done
 
+#Last name check
+lname_check=0
+echo "Enter last name starts with Caps and has minimum 3 Characters"
+while [[ lname_check -eq 0 ]]
+do
+        read -p "Enter Last name: " lname
+        if [[ $lname =~ $pattern_name ]]
+        then
+                lname_check=1
+        else
+                echo "Please enter valid last name"
+        fi
+done
+
+
 #show user data
 echo "<--------- User Information --------->"
 echo "First Name: " $fname
+echo "Last Name: " $lname
