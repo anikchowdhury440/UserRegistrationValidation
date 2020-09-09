be2c13a3297d8a2c40a7d03f50e407ffee3325f3
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-09-09 15:31:11 +0530

    User First Name Validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,22 @@
 #!/bin/bash -x
 
+#pattern
+pattern_fname="^[[:upper:]][[:lower:]]{2,}$"
+
+#First name check
+fname_check=0
+echo "Enter first name starts with Caps and has minimum 3 Characters"
+while [[ fname_check -eq 0 ]]
+do
+        read -p "Enter First name: " fname
+        if [[ $fname =~ $pattern_fname ]]
+        then
+                fname_check=1
+        else
+                echo "Please enter valid first name"
+        fi
+done
+
+#show user data
+echo "<--------- User Information --------->"
+echo "First Name: " $fname
